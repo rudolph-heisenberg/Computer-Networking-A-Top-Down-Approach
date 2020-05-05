cc31f91984b75729448f418abdbb93e816421c73
rudolph-heisenberg <kissyekechan123@gmail.com>
2020-05-05 12:47:28 +0800

    minor modifications

--- 我自己的代码/SocketProgrammingAssignment/作业1-Web服务器/附加作业：客户端编写/client.py
+++ 我自己的代码/SocketProgrammingAssignment/作业1-Web服务器/附加作业：客户端编写/client.py
@@ -2,12 +2,13 @@ import sys
 
 from socket import *
 
-serverName = '127.0.0.1'
-serverPort = 8044
+serverName = sys.argv[1]
+serverPort = int(sys.argv[2])
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
 
-requestMessage = 'GET /1.html HTTP/1.1\r\n\r\n'
+requestMessage = 'GET /'+ sys.argv[3] +' HTTP/1.1\r\n\r\n'
 clientSocket.send(requestMessage.encode())
 response = clientSocket.recv(1024).decode()
-print(response)
+storeFile = open('storeFile.txt',mode='w')
+storeFile.write(response)
